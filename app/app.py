8070e2941578f466f6c594726293fa5a8e500f65
Takenori Takaki <takenory@gmail.com>
2023-06-09 15:24:13 +0900

    Add LangChain agent code example

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.llms import OpenAI
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "index page"
+  llm = OpenAI(temperature=0)
+  tools = load_tools(["serpapi", "llm-math"], llm=llm)
+  agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+  response = agent.run("FlaskというWebアプリケーションフレームワークが提供する機能をマインドマップにまとめて、PlantUMLで出力してください。")
+
+  return response
