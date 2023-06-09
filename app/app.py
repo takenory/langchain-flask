4ec8065f93974eae56a30d0e241fdcd4d3ea1fe5
Takenori Takaki <takenory@gmail.com>
2023-06-09 19:13:44 +0900

    Enable to generate chain prompt using Web form

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 from langchain.llms import OpenAI
@@ -8,10 +8,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+  return render_template('index.html')
+
+@app.route('/chain', methods=['post'])
+def chain():
   llm = OpenAI(streaming=True, callbacks=[FinalStreamingStdOutCallbackHandler()], temperature=0)
   tools = load_tools(["serpapi", "llm-math"], llm=llm)
   agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
-  prompt = request.args.get('prompt')
-  response = agent.run(prompt)
+  # prompt = request.args.get('prompt')
+  # response = agent.run(prompt)
+  instruction = request.form.get('instruction')
+  output_format = request.form.get('output_format')
 
-  return response
+  prompt = instruction + 'について、Web上の最新情報でリサーチした内容を元に日本語で'
+  if output_format == 'mindmap':
+    diagram = agent.run(prompt + 'マインドマップにまとめて、PlantUMLで出力してください。')
+    return render_template('mindmap.html', diagram = diagram)
+  else:
+    response = agent.run(prompt + 'プレーンテキストでまとめて、出力してください。')
+    return response
