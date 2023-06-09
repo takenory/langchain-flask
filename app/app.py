25a9cab6922a78e67c92a14b196566e9b86ec4ea
Takenori Takaki <takenory@gmail.com>
2023-06-09 16:19:13 +0900

    Change way of agent response

--- app/app.py
+++ app/app.py
@@ -2,14 +2,15 @@ from flask import Flask
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 from langchain.llms import OpenAI
+from langchain.callbacks.streaming_stdout_final_only import FinalStreamingStdOutCallbackHandler
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  llm = OpenAI(temperature=0)
+  llm = OpenAI(streaming=True, callbacks=[FinalStreamingStdOutCallbackHandler()], temperature=0)
   tools = load_tools(["serpapi", "llm-math"], llm=llm)
   agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
-  response = agent.run("FlaskというWebアプリケーションフレームワークが提供する機能をマインドマップにまとめて、PlantUMLフォーマットのテキストデータとして出力してください。")
+  response = agent.run("FlaskというWebアプリケーションフレームワークが提供する機能をマインドマップにまとめて、PlantUMLのテキストデータとして生成してください。")
 
   return response
