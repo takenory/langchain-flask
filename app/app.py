4cac8bd44fe8d1db8a764266414676d6d04713b9
Takenori Takaki <takenory@gmail.com>
2023-06-13 08:31:34 +0900

    Refactor app script

--- app/app.py
+++ app/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 from langchain.llms import OpenAI
-from langchain.callbacks.streaming_stdout_final_only import FinalStreamingStdOutCallbackHandler
 
 app = Flask(__name__)
 
@@ -12,19 +11,15 @@ def index():
 
 @app.route('/chain', methods=['post'])
 def chain():
-  llm = OpenAI(streaming=True, callbacks=[FinalStreamingStdOutCallbackHandler()], temperature=0)
-  #llm = OpenAI(streaming=True, temperature=0)
+  llm = OpenAI(streaming=True, temperature=0)
   tools = load_tools(["google-search"], llm=llm)
   agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
-  # prompt = request.args.get('prompt')
-  # response = agent.run(prompt)
   instruction = request.form.get('instruction')
   output_format = request.form.get('output_format')
 
-  prompt = instruction + 'について、Web上の最新情報でリサーチした内容を日本語で'
   if output_format == 'mindmap':
-    diagram = agent.run(prompt + 'マインドマップにまとめて、@startmindmapではじまり@endmindmapで終わるPlantUMLで出力してください。')
+    diagram = agent.run(instruction + 'について日本語でマインドマップにまとめて、@startmindmapではじまり@endmindmapで終わるPlantUMLで出力してください。')
     return render_template('mindmap.html', diagram = diagram)
   else:
-    response = agent.run(prompt + 'プレーンテキストでまとめて、出力してください。')
+    response = agent.run(prompt + 'について日本語でプレーンテキストでまとめて、出力してください。')
     return response
